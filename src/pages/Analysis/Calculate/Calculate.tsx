81b738cbcde747607ba7e16ceec022aaac423ac1
Sthae <sthae38@gmail.com>
2021-05-06 14:04:38 +0200

    implemented the basic mock up for the calculate page

--- src/pages/Analysis/Calculate/Calculate.tsx
+++ src/pages/Analysis/Calculate/Calculate.tsx
@@ -1,14 +1,64 @@
-import { Container, Typography } from "@material-ui/core";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Container, Grid, TextField, Typography } from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
+import useStyles from './styles';
+
+const budget_retrack = 'Calculate the required usage rate In order to get back on budget within the specified number of days below';
+const package_finish = 'Calculate the required usage rate in order to finish the current data within the specified number of days';
+
+interface CalculationSectionProps
+{
+    description: string;
+    heading: string;
+}
 
 function Calculate()
 {
+    const styles = useStyles();
     return (
         <Container>
-            <Typography variant='h4'>This is the calculate page</Typography>
+            <Typography variant='h5' align='center' className={styles.title} >Use these sections to do utility calculations</Typography>
+            <CalculationSection heading='Get back on track' description={budget_retrack} />
+
+            <CalculationSection heading='Finish a packge' description={package_finish} />
         </Container>
     )
 }
 
+function CalculationSection({ description, heading }: CalculationSectionProps)
+{
+    const styles = useStyles()
+    return (
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} id="panel1a-header" >
+                <Typography className={styles.heading}>{heading}</Typography>
+            </AccordionSummary>
+
+            <AccordionDetails>
+                <Typography variant='h6'>{description}</Typography>
+                <CalculationForm />
+            </AccordionDetails>
+        </Accordion>
+    )
+}
+
+function CalculationForm()
+{
+    const styles = useStyles();
+    return (
+        <Grid container className={styles.container} spacing={2}>
+            <Grid item xs={12} className={styles.flex_container}>
+                <div className={styles.field}>
+                    <TextField label='Days' fullWidth />
+                </div>
+                <Button className={styles.button} variant='contained' color='primary'> Calculate</Button>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Typography variant='h6'>The required usage rate is 0.54GB</Typography>
+            </Grid>
+        </Grid>
+    )
+}
 
 export default Calculate;
--- src/pages/Analysis/Calculate/styles.ts
+++ src/pages/Analysis/Calculate/styles.ts
@@ -1,6 +1,35 @@
 import { makeStyles } from "@material-ui/core";
 
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles(theme => ({
+    title: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(2)
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+    flex_container: {
+        display: 'flex'
+    },
+    field: {
+        width: '70%'
+    },
+    paper: {
+        width: '80%',
+        height: 100
+    },
+    container: {
+        width: '50%',
+        marginTop: theme.spacing(3),
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    button: {
+        marginTop: theme.spacing(2),
+        marginLeft: theme.spacing(2)
+    }
+}));
 
 export default useStyles;
